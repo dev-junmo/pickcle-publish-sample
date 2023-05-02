6c1090bf60df768558caee16f2fb48c787669755
chojunmo <chojunmo@chojunmoui-MacBookAir.local>
2023-05-02 22:16:04 +0900

    Swiper 적용

--- script.js
+++ script.js
@@ -43,8 +43,8 @@ function nextMove() {
     slideItems.forEach((i) => {
       i.setAttribute("style", `left: ${-offset}px`);
     });
-    indicationItems.forEach((i) => i.classList.remove("active"));
-    indicationItems[currSlide - 1].classList.add("active");
+    indicationItems.forEach((i) => i.classList.remove("on"));
+    indicationItems[currSlide - 1].classList.add("on");
   } else {
     currSlide = 0;
     let offset = slideWidth * currSlide;
@@ -58,7 +58,93 @@ function nextMove() {
         i.setAttribute("style", `transition: ${0.15}s; left: ${-offset}px`);
       });
     }, 0);
-    indicationItems.forEach((i) => i.classList.remove("active"));
-    indicationItems[currSlide - 1].classList.add("active");
+    indicationItems.forEach((i) => i.classList.remove("on"));
+    indicationItems[currSlide - 1].classList.add("on");
   }
 }
+
+function prevMove() {
+  currSlide--;
+  if (currSlide > 0) {
+    const offset = slideWidth * currSlide;
+    slideItems.forEach((i) => {
+      i.setAttribute("style", `left: ${-offset}px`);
+    });
+    indicationItems.forEach((i) => i.classList.remove("on"));
+    indicationItems[currSlide - 1].classList.add("on");
+  } else {
+    currSlide = maxSlide + 1;
+    let offset = slideWidth * currSlide;
+    slideItems.forEach((i) => {
+      i.setAttribute("style", `transition: ${0}s; left: ${-offset}px`);
+    });
+    currSlide--;
+    offset = slideWidth * currSlide;
+    setTimeout(() => {
+      slideItems.forEach((i) => {
+        i.setAttribute("style", `transition: ${0.15}s; left: ${-offset}px`);
+      });
+    }, 0);
+    indicationItems.forEach((i) => i.classList.remove("on"));
+    indicationItems[currSlide - 1].classList.add("on");
+  }
+}
+
+window.addEventListener("resize", () => {
+  slideWidth = slides.clientWidth;
+});
+
+for (let i = 0; i < maxSlide; i++) {
+  indicationItems[i].addEventListener("click", () => {
+    currSlide = i + 1;
+    const offset = slideWidth * currSlide;
+    slideItems.forEach((i) => {
+      i.setAttribute("style", `left: ${-offset}px`);
+    });
+    indicationItems.forEach((i) => i.classList.remove("on"));
+    indicationItems[currSlide - 1].classList.add("on");
+  });
+}
+
+
+let startPoint = 0;
+let endPoint = 0;
+
+slides.addEventListener("mousedown", (e) => {
+  startPoint = e.pageX;
+});
+
+slides.addEventListener("mouseup", (e) => {
+  endPoint = e.pageX;
+  if (startPoint < endPoint) {
+    prevMove();
+  } else if (startPoint > endPoint) {
+    nextMove();
+  }
+});
+
+slides.addEventListener("touchstart", (e) => {
+  startPoint = e.touches[0].pageX;
+});
+slides.addEventListener("touchend", (e) => {
+  endPoint = e.changedTouches[0].pageX;
+  if (startPoint < endPoint) {
+    prevMove();
+  } else if (startPoint > endPoint) {
+    nextMove();
+  }
+});
+
+let loopInterval = setInterval(() => {
+  nextMove();
+}, 3000);
+
+slides.addEventListener("mouseover", () => {
+  clearInterval(loopInterval);
+});
+
+slides.addEventListener("mouseout", () => {
+  loopInterval = setInterval(() => {
+    nextMove();
+  }, 3000);
+});
